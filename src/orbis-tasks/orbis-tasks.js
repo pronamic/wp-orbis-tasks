60ccb0ec8424fbf363260107e5156fc432dcb3d9
Remco Tolsma <info@remcotolsma.nl>
2014-10-14 16:54:12 +0200

    Renamed todos to tasks.

--- src/orbis-tasks/orbis-tasks.js
+++ src/orbis-tasks/orbis-tasks.js
@@ -1,24 +1,49 @@
 'use strict';
 
 orbisApp.controller( 'OrbisTasksCtrl', function( $scope ) {
-  $scope.todos = [
-    {text:'Learn AngularJS', done:false},         
-    {text: 'Build an app', done:false}
-  ];
+	$scope.tasks = [
+		{
+			text: 'Learn AngularJS',
+			date: '2014-01-01',
+			time: 1800,
+			done: false
+		},         
+		{
+			text: 'Build an app',
+			date: '2014-02-02',
+			time: 3600,
+			done: false
+		}
+	];
   
-  $scope.getTotalTodos = function () {
-    return $scope.todos.length;
-  };
+	$scope.getTotalTasks = function() {
+		return $scope.tasks.length;
+	};
+
+	$scope.addTask = function() {
+		var time = 0;
+		if ( angular.isNumber( $scope.formTaskHours ) ) {
+			time += $scope.formTaskHours * 3600;
+		}
+		if ( angular.isNumber( $scope.formTaskMinutes ) ) {
+			time += $scope.formTaskMinutes * 60;
+		}
+
+		var task = {
+			text: $scope.formTaskText,
+			date: $scope.formTaskDate,
+			time: time,
+			done: false
+		};
+
+		$scope.tasks.push( task );
+
+		$scope.formTaskText = '';
+	};
   
-  
-  $scope.addTodo = function () {
-    $scope.todos.push({text:$scope.formTodoText, done:false});
-    $scope.formTodoText = '';
-  };
-  
-    $scope.clearCompleted = function () {
-        $scope.todos = $scope.todos.filter( function(todo){
-            return !todo.done;
-        });
+	$scope.clearCompleted = function() {
+		$scope.tasks = $scope.tasks.filter( function( task ) {
+			return ! task.done;
+        } );
     };
 } );
