f51b56b6ab8c744020b9b61f81186faffd31bbb8
Ruben Droogh <rubendroogh@iMac-van-Leo.local>
2018-02-06 11:06:52 +0100

    Added phpcs and ignore vendor folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,23 @@ module.exports = function( grunt ) {
 		// Package
 		pkg: grunt.file.readJSON( 'package.json' ),
 		
+		dirs: {
+			ignore: [ 'build', 'node_modules', 'vendor' ].join( ',' ) 
+		},
+
+		// PHP Code Sniffer
+		phpcs: {
+			application: {
+				dir: [ '.' ],
+			},
+			options: {
+				bin: 'vendor/bin/phpcs',
+				standard: 'phpcs.ruleset.xml',
+				extensions: 'php',
+				ignore: '<%= dirs.ignore %>',
+			}
+		},
+
 		// PHPLint
 		phplint: {
 			options: {
@@ -44,6 +61,7 @@ module.exports = function( grunt ) {
 		}
 	} );
 
+	grunt.loadNpmTasks( 'grunt-phpcs' );
 	grunt.loadNpmTasks( 'grunt-phplint' );
 	grunt.loadNpmTasks( 'grunt-checkwpversion' );
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
