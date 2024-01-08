cda87e169264f6ab613d31225236add43879e3f0
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-01-08 15:55:26 +0100

    Update orbis-tasks.php

--- orbis-tasks.php
+++ orbis-tasks.php
@@ -1,23 +1,37 @@
 <?php
-/*
-Plugin Name: Orbis Tasks
-Plugin URI: http://www.orbiswp.com/
-Description: The Orbis Tasks plugin extends your Orbis environment with the option to add tasks and connect them to Orbis projects.
+/**
+ * Orbis Tasks
+ *
+ * @package   Pronamic\Orbis\Tasks
+ * @author    Pronamic
+ * @copyright 2024 Pronamic
+ * @license   GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Orbis Tasks
+ * Plugin URI:        https://wp.pronamic.directory/plugins/orbis-tasks/
+ * Description:       The Orbis Tasks plugin extends your Orbis environment with the option to add tasks and connect them to Orbis projects.
+ * Version:           1.1.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Pronamic
+ * Author URI:        https://www.pronamic.eu/
+ * Text Domain:       orbis-tasks
+ * Domain Path:       /languages/
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://wp.pronamic.directory/plugins/orbis-tasks/
+ * GitHub URI:        https://github.com/pronamic/wp-orbis-tasks
+ */
 
-Version: 1.1.1
-Requires at least: 3.5
+/**
+ * Autoload.
+ */
+require_once __DIR__ . '/vendor/autoload_packages.php';
 
-Author: Pronamic
-Author URI: http://www.pronamic.eu/
-
-Text Domain: orbis_tasks
-Domain Path: /languages/
-
-License: Copyright (c) Pronamic
-
-GitHub URI: https://github.com/pronamic/wp-orbis-tasks
-*/
-
+/**
+ * Bootstrap.
+ */
 function orbis_tasks_bootstrap() {
 	// Classes
 	require_once 'classes/orbis-task.php';
